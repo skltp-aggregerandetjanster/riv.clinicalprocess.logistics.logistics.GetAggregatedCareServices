c2797f786acd24b12865dfaee34331c2c747805f
hanswiklund <hans.wiklund@kentor.se>
2016-09-20 09:37:49 +0200

    gatling

--- test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcareservices/CommonParameters.scala
+++ test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcareservices/CommonParameters.scala
@@ -2,12 +2,12 @@ package se.skltp.aggregatingservices.riv.clinicalprocess.logistics.logistics.get
 
 trait CommonParameters {
   val serviceName:String     = "CareServices"
-  val urn:String             = "urn:riv:clinicalprocess:logistics:logistics:GetCareServicesResponder:3"
+  val urn:String             = "urn:riv:clinicalprocess:logistics:logistics:GetCareServicesResponder:2"
   val responseElement:String = "GetCareServicesResponse"
   val responseItem:String    = "careContact"
   var baseUrl:String         = if (System.getProperty("baseUrl") != null && !System.getProperty("baseUrl").isEmpty()) {
                                    System.getProperty("baseUrl")
                                } else {
-                                   "http://33.33.33.33:8081/GetAggregatedCareServices/service/v3"
+                                   "http://33.33.33.33:8081/GetAggregatedCareServices/service/v2"
                                }
 }
