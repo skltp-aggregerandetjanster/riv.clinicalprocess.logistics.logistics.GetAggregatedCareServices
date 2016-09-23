f50cfff827c5f5310d13cf2a22d96fe65968ed01
hanswiklund <hans.wiklund@kentor.se>
2016-09-23 12:24:51 +0200

    gatling tests

--- test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcareservices/CommonParameters.scala
+++ test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcareservices/CommonParameters.scala
@@ -4,7 +4,7 @@ trait CommonParameters {
   val serviceName:String     = "CareServices"
   val urn:String             = "urn:riv:clinicalprocess:logistics:logistics:GetCareServicesResponder:2"
   val responseElement:String = "GetCareServicesResponse"
-  val responseItem:String    = "careContact"
+  val responseItem:String    = "careService"
   var baseUrl:String         = if (System.getProperty("baseUrl") != null && !System.getProperty("baseUrl").isEmpty()) {
                                    System.getProperty("baseUrl")
                                } else {
